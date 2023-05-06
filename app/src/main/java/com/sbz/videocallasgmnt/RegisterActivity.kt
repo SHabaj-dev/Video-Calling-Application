1ced84bc6860590eb60365a0400bb8aa97e7f391
SHabaj-dev <shabajansari843@gmail.com>
2023-05-07 01:10:23 +0530

    Added register Page

--- app/src/main/java/com/sbz/videocallasgmnt/RegisterActivity.kt
+++ app/src/main/java/com/sbz/videocallasgmnt/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.sbz.videocallasgmnt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        setStatusBarProperties()
+    }
+
+    private fun setStatusBarProperties() {
+        val window: Window = window
+        val decorView: View = window.decorView
+        val wic = WindowInsetsControllerCompat(window, decorView)
+        wic.isAppearanceLightStatusBars = true
+        window.statusBarColor = Color.WHITE
     }
 }
