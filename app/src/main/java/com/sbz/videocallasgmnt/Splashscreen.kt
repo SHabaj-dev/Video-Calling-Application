f41dc4254c1aed9a262bcaa282b111d24601f76d
SHabaj-dev <shabajansari843@gmail.com>
2023-05-06 23:14:35 +0530

    implmented splash screen

---
+++ app/src/main/java/com/sbz/videocallasgmnt/Splashscreen.kt
@@ -0,0 +1,26 @@
+package com.sbz.videocallasgmnt
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class Splashscreen : AppCompatActivity() {
+    private val SCREEN_TIME_OUT = 2000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        supportActionBar?.hide()
+        window.statusBarColor = Color.WHITE
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this@Splashscreen, MainActivity::class.java))
+                finish()
+            },
+            SCREEN_TIME_OUT
+        )
+    }
+}
